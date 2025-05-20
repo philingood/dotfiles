179b3b36e3eb3aa5d7e56e9664cd8bef27768f35
philingood <98781376+philingood@users.noreply.github.com>
2025-05-20 20:51:46 +0300

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-stow .
+
+if [ -z "$NIX_PATH" ]; then
+    # If NIX_PATH is not set, use stow without .stowrc
+    stow --ignore='.stowrc' --ignore='.DS_Store' --ignore='README.md' .
+else
+    # If NIX_PATH is set, use stow with settings from .stowrc
+    stow .
+fi
