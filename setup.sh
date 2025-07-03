44829737f228d69140ba9852946f14186e5eaec5
philingood <98781376+philingood@users.noreply.github.com>
2025-07-03 19:55:58 +0300

    Fix github workflows

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -z "$NIX_PATH" ]; then
-    # If NIX_PATH is not set, use stow without .stowrc
-    stow --ignore='.stowrc' --ignore='.DS_Store' --ignore='README.md' .
+  # If NIX_PATH is not set, use stow without .stowrc
+  stow --ignore='.stowrc' --ignore='.github' --ignore='.DS_Store' --ignore='README.md' .
 else
-    # If NIX_PATH is set, use stow with settings from .stowrc
-    stow .
+  # If NIX_PATH is set, use stow with settings from .stowrc
+  stow .
 fi
